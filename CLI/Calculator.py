18ae20d7c5a84a3894895480e723175505a55b05
“CloCloudKylin <zc0k27@gmail.com>
2018-06-11 19:47:38 +0800

    Change algorithm.

--- CLI/Calculator.py
+++ CLI/Calculator.py
@@ -1,71 +1,85 @@
 import itertools
 
-class analysis:
+
+class Analysis:
     def __init__(self, x, y):
-        #固定参数
+        # 固定参数
         self.money = {6: 10000, 7: 36, 8: 720, 9: 360, 10: 80, 11: 252,
-                 12: 108, 13: 72, 14: 54, 15: 180, 16: 72, 17: 180,
-                 18: 119, 19: 36, 20: 306, 21: 1080, 22: 144, 23: 1800, 24: 3600}
+                      12: 108, 13: 72, 14: 54, 15: 180, 16: 72, 17: 180,
+                      18: 119, 19: 36, 20: 306, 21: 1080, 22: 144, 23: 1800, 24: 3600}
         self.way = ['第一行', '第二行', '第三行', '第一列', '第二列', '第三列', '左上至右下', '右上至左下']
         self.num = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-        self.x = x
-        self.y = y
+        self.x = x  # 位置坐标
+        self.y = y  # 数
 
-        #计算变量
-        self.linsam = [[] for i in range(8)]
-        self.linmoney = [[] for i in range(8)]
+        # 计算变量
+        self.lineSum = [[] for i in range(8)]
+        self.lineMoney = [[] for i in range(8)]
+        self.lineStat = [dict() for i in range(8)]
 
         for i in range(4):
-            self.num.remove(y[i])
-        for i in itertools.permutations(self.num, 5):
+            self.num.remove(y[i])  # 从1-9中移除已知的数
+        for i in itertools.permutations(self.num, 5):  # 全排列
             comb = list(i)
             for j in range(4):
-                comb.insert(x[j], y[j])
+                comb.insert(x[j], y[j])  # 将已知的数插入对应位置
             self.cal(comb)
 
-    def cal(self,list):
-        #计算每一行的总合
-        self.linsam[0].append(list[0] + list[1] + list[2])
-        self.linsam[1].append(list[3] + list[4] + list[5])
-        self.linsam[2].append(list[6] + list[7] + list[8])
-        self.linsam[3].append(list[0] + list[3] + list[6])
-        self.linsam[4].append(list[1] + list[4] + list[7])
-        self.linsam[5].append(list[2] + list[5] + list[8])
-        self.linsam[6].append(list[0] + list[4] + list[8])
-        self.linsam[7].append(list[2] + list[4] + list[6])
+    def cal(self, list):
+        # 计算每种情况的总合
+        self.lineSum[0].append(list[0] + list[1] + list[2])  # 第一行
+        self.lineSum[1].append(list[3] + list[4] + list[5])  # 第二行
+        self.lineSum[2].append(list[6] + list[7] + list[8])  # 第三行
+        self.lineSum[3].append(list[0] + list[3] + list[6])  # 第一列
+        self.lineSum[4].append(list[1] + list[4] + list[7])  # 第二列
+        self.lineSum[5].append(list[2] + list[5] + list[8])  # 第三列
+        self.lineSum[6].append(list[0] + list[4] + list[8])  # 左上至右下
+        self.lineSum[7].append(list[2] + list[4] + list[6])  # 右上至左下
 
-        #计算每一行的奖励
+        # 计算每种情况的奖励
         for i in range(8):
-            num = self.linsam[i][-1]
-            self.linmoney[i].append(self.money[num])
-
-    def bestline(self):
-        avgmoney = []
-        maxmoney = 0
-        maxmoneyline = 0
-        for i in range(8):
-            avgmoney.append(sum(self.linmoney[i]) / len(self.linmoney[i]))
-            if avgmoney[i] > float(maxmoney):
-                maxmoney = avgmoney[i]
-                maxmoneyline = i
-        return '预计选择 ' + self.way[maxmoneyline] + ' 获得的金蝶币最多，为 ' + str(maxmoney)
-
-    def maxmoneyline(self):
-        maxmoneyNum = (6, 24, 23)
-        msg = ''
-        for i in range(3):
-            maxmoneyProbability = 0
-            maxmoneyLine = 0
-            for j in range(8):
-                time = self.linsam[j].count(maxmoneyNum[i])
-                if time > maxmoneyProbability:
-                    maxmoneyProbability = time
-                    maxmoneyLine = j
-            if maxmoneyProbability == 0:
-                msg += '没有可能获得 %d 金蝶币。\n'% self.money[maxmoneyNum[i]]
+            num = self.lineSum[i][-1]
+            money = self.money[num]
+            self.lineMoney[i].append(money)
+            if self.lineStat[i].get(money) is not None:
+                self.lineStat[i][money] = self.lineStat[i].get(money) + 1
             else:
-                msg += '选择 %s 最有可能获得 %d 金蝶币，概率为 %d/120。\n'% (self.way[maxmoneyLine], self.money[maxmoneyNum[i]], maxmoneyProbability)
+                self.lineStat[i][self.money[num]] = 1
+
+    def maxavg(self):
+        maxavg_money = 0
+        maxavg_way = 0
+        thisway = 0
+        for line in self.lineStat:  # 获取每一行的统计结果
+            sum = 0
+            for money, times in line.items():
+                sum += money * times
+            sum /= 120  # 求每一行的平均值
+            if sum > maxavg_money:  # 判断是否更新最大值
+                maxavg_money = sum
+                maxavg_way = thisway
+            thisway += 1  # 计数
+        return '预计选择 %s 获得的金蝶币最多，为 %d' % (self.way[maxavg_way], maxavg_money)
+
+    def mostmoney(self):
+        neednum = (10000, 3600, 1800)
+        msg = ''
+        for money in neednum:
+            maxtimes_money = 0
+            maxtimes_way = 0
+            thisway = 0
+            for line in self.lineStat:  # 获取每一行的统计结果
+                if line.get(money, 0) > maxtimes_money:  # 判断是否需要更新最大值
+                    maxtimes_money = line.get(money)
+                    maxtimes_way = thisway
+                thisway += 1
+            if maxtimes_money == 0:  # 判断是否有需要的钱数
+                msg += '没有可能获得 %d 金蝶币。\n' % money
+            else:
+                msg += '选择 %s 最有可能获得 %d 金蝶币，概率为 %d/120。\n' % (
+                    self.way[maxtimes_way], money, maxtimes_money)
         return msg
+
 
 if __name__ == "__main__":
     x = []
@@ -74,7 +88,7 @@ if __name__ == "__main__":
         x.append(int(input("位置：")))
         y.append(int(input("数：")))
         x[-1] -= 1
-    num = analysis(x, y)
-    print(num.bestline())
-    print(num.maxmoneyline())
+    num = Analysis(x, y)
+    print(num.maxavg())
+    print(num.mostmoney())
     input('按回车键退出...')
--- GUI/main.py
+++ GUI/main.py
@@ -3,10 +3,11 @@ import tkinter.messagebox
 from tkinter import *
 import ffcalculator
 
+
 class MainWindow(Tk):
     def __init__(self):
         super().__init__()
-        self.pos = [[]for i in range(9)]
+        self.pos = [[] for i in range(9)]
         for i in range(9):
             self.pos[i] = IntVar()
 
@@ -16,7 +17,7 @@ class MainWindow(Tk):
     def GUI(self):
         window = Frame(self)
         window.pack()
-        Label(window,text="从左上到右下").grid(row=0, column=0)
+        Label(window, text="从左上到右下").grid(row=0, column=0)
         Label(window, text="第一列").grid(row=0, column=1)
         Label(window, text="第二列").grid(row=0, column=2)
         Label(window, text="第三列").grid(row=0, column=3)
@@ -50,50 +51,51 @@ class MainWindow(Tk):
     def Bestline_Button(self):
         x = []
         y = []
-        for i in  range(9):
+        for i in range(9):
             num = self.pos[i].get()
             if num == 0:
                 continue
             elif num > 9 or num < 0:
-                tkinter.messagebox.showerror("输入错误", "位置 %d 的数字输错啦，快去看看是不是手抖啦？。" % (i+1))
+                tkinter.messagebox.showerror("输入错误", "位置 %d 的数字输错啦，快去看看是不是手抖啦？。" % (i + 1))
                 return
             elif num in y:
-                tkinter.messagebox.showerror("输入重复", "位置 %d 的数字输重啦，快去看看是不是眼花啦？" % (i+1))
+                tkinter.messagebox.showerror("输入重复", "位置 %d 的数字输重啦，快去看看是不是眼花啦？" % (i + 1))
                 return
             else:
                 x.append(i)
                 y.append(num)
-        if len(x) != 4 or len(y) !=4:
+        if len(x) != 4 or len(y) != 4:
             tkinter.messagebox.showerror("超过限制", "你好像没开对格子，要开好4个再来找窝哟~多开少开都不行哟~")
-        cal = ffcalculator.analysis(x,y)
-        msg = cal.bestline()
+        cal = ffcalculator.Analysis(x, y)
+        msg = cal.maxavg()
         tkinter.messagebox._show("提交成功", msg)
 
     def Maxmoney_Button(self):
         x = []
         y = []
-        for i in  range(9):
+        for i in range(9):
             num = self.pos[i].get()
             if num == 0:
                 continue
             elif num > 9 or num < 0:
-                tkinter.messagebox.showerror("输入错误", "位置 %d 的数字输错啦，快去看看是不是手抖啦？。" % (i+1))
+                tkinter.messagebox.showerror("输入错误", "位置 %d 的数字输错啦，快去看看是不是手抖啦？。" % (i + 1))
                 return
             elif num in y:
-                tkinter.messagebox.showerror("输入重复", "位置 %d 的数字输重啦，快去看看是不是眼花啦？" % (i+1))
+                tkinter.messagebox.showerror("输入重复", "位置 %d 的数字输重啦，快去看看是不是眼花啦？" % (i + 1))
                 return
             else:
                 x.append(i)
                 y.append(num)
-        if len(x) != 4 or len(y) !=4:
+        if len(x) != 4 or len(y) != 4:
             tkinter.messagebox.showerror("超过限制", "你好像没开对格子，要开好4个再来找窝哟~多开少开都不行哟~")
-        cal = ffcalculator.analysis(x,y)
-        msg = cal.maxmoneyline()
+        cal = ffcalculator.Analysis(x, y)
+        msg = cal.mostmoney()
         tkinter.messagebox._show("提交成功", msg)
 
     def Reset_Button(self):
         for i in range(9):
             self.pos[i].set(0)
+
 
 if __name__ == "__main__":
     app = MainWindow()
